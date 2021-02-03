aef366cffcc1a75747ad9d9b700bb982178b2f04
YLakshmiPratyusha <42644237+YLakshmiPratyusha@users.noreply.github.com>
2021-02-03 16:04:04 +0530

    Update ProductService.java

--- onlinegrocery/src/main/java/com/cg/onlinegrocery/service/ProductService.java
+++ onlinegrocery/src/main/java/com/cg/onlinegrocery/service/ProductService.java
@@ -1,6 +1,14 @@
 package com.cg.onlinegrocery.service;
 
-public interface ProductService {
+import java.util.List;
+import com.cg.onlinegrocery.domain.Product;
 
+public interface ProductService {
+	public void addProduct(Product product);
+	public void deleteProduct(int productId, String productName);
+	public void searchProduct(String productName, int productId);
+	public void updateProduct(Product product1, int productId);
+	public List<Product> viewAllProducts(Product products);
+	
 
 }
